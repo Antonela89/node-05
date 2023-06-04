742cd0e75c4d28c38acdc64cb276d3c5901e62bb
Antonela Borgogno <antonelaborgogno@gmail.com>
2023-06-03 21:04:14 -0300

    correcion ruta en action en edit.html, correcion de app.use de api/productos en index.js. Creacion de nuevo metodo update en api

--- src/routes/api/productosApiRouter.js
+++ src/routes/api/productosApiRouter.js
@@ -6,4 +6,9 @@ router.post("/create", (req, res) => { //recibe un json
     res.send('producto creado con Api' + req.body.nombre);
 })
 
+router.put("/update", (req, res) => {
+    console.log(req.body);
+    res.send(`producto editado: ${req.body.nombre}, nuevo precio: ${req.body.precio}`);
+})
+
 module.exports = router;
